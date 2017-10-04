f57797ee5cca6f82b301cd2f7303765ed73797a6
Thomas Løkkeborg <thomahl@stud.ntnu.no>
2017-10-04 16:22:51 +0200

    env fix

--- main.go
+++ main.go
@@ -39,22 +39,27 @@ func main() {
 
 	if scheduleHour, err = envAsInt("SCHEDULE_HOUR"); err != nil {
 		fmt.Println("Env error: (", err.Error(), ")")
+		return
 	}
 
 	if scheduleMinute, err = envAsInt("SCHEDULE_MINUTE"); err != nil {
 		fmt.Println("Env error: (", err.Error(), ")")
+		return
 	}
 
 	if scheduleSecond, err = envAsInt("SCHEDULE_SECOND"); err != nil {
 		fmt.Println("Env error: (", err.Error(), ")")
+		return
 	}
 
-	if token, err = envAsString("TOKEN"); err != nil {
+	if token, err = envAsString("API_TOKEN"); err != nil {
 		fmt.Println("Env error: (", err.Error(), ")")
+		return
 	}
 
-	if path, err = envAsString("TOKEN"); err != nil {
+	if path, err = envAsString("PATH_TO_ICS"); err != nil {
 		fmt.Println("Env error: (", err.Error(), ")")
+		return
 	}
 
 	// print so it shows up in the logs
